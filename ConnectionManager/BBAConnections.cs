7931f4afafd658e20679453953e334fc2e3281ec
tridip-bba <tridip@bba-reman.com>
2016-06-29 20:40:17 +0530

    sample code added

---
+++ ConnectionManager/BBAConnections.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.Practices.Unity;
+
+namespace ConnectionManager
+{
+
+    public interface IBBAConnection
+    {
+        IDbConnection  GetConnection();
+        string ConType { get; set; }
+    }
+
+    public class BBAConnection : IBBAConnection
+    {
+        public string ConType { get; set; }
+
+        public IDbConnection GetConnection()
+        {
+            string _connectionString = "";
+            IDbConnection connection = null;
+
+            try
+            {
+                // inside if else logic we fetch connection string from ini file or from any source and inistialize connection.
+                if (ConType == "local")
+                {
+                    _connectionString = "put here local db connection";
+                    connection = new System.Data.SqlClient.SqlConnection(_connectionString);
+                }
+                else if (ConType == "remote")
+                {
+                    _connectionString = "put here remote db connection";
+                    connection = new System.Data.SqlClient.SqlConnection(_connectionString);
+                }
+                else if (ConType == "OrcsWeb")
+                {
+                    _connectionString = "put here website db connection";
+                    connection = new System.Data.SqlClient.SqlConnection(_connectionString);
+                }
+                else if (ConType == "Sage")
+                {
+                    _connectionString = "put here sage connection";
+                    connection = new System.Data.SqlClient.SqlConnection(_connectionString);
+                }
+
+                connection.Open();
+            }
+            catch (Exception ex)
+            {
+                string strErr = ex.Message;
+            }
+
+            return connection;
+        }
+    }
+
+    public static class Factory
+    {
+        static IUnityContainer cont = null;
+
+        public static IBBAConnection initialize(string type)
+        {
+            IBBAConnection oDbConnection = null;
+
+            cont = new UnityContainer();
+            cont.RegisterType<IBBAConnection, BBAConnection>("local");
+            cont.RegisterType<IBBAConnection, BBAConnection>("remote");
+            cont.RegisterType<IBBAConnection, BBAConnection>("OrcsWeb");
+            cont.RegisterType<IBBAConnection, BBAConnection>("Sage");
+
+            oDbConnection = cont.Resolve<IBBAConnection>(type);
+            oDbConnection.ConType = type;
+
+            return oDbConnection;
+        }
+    }
+
+}
---
+++ ConnectionManager/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConnectionManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Factory.initialize("OrcsWeb").GetConnection().State == System.Data.ConnectionState.Open)
+            {
+                MessageBox.Show("Connection open");
+            }
+            else
+            {
+                MessageBox.Show("Connection close");
+            }
+        }
+    }
+}
